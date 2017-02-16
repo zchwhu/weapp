78504866291ba1ad58b45174ff4e1d2de7e3ae49
zch0301 <291053953@qq.com>
2017-02-16 14:31:14 +0800

    设置页面跳转事件

--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -1,3 +1,16 @@
 Page({
-    
+    onTap:function(){
+        wx.redirectTo({
+          url: '../posts/post',
+          success: function(res){
+            // success
+          },
+          fail: function() {
+            // fail
+          },
+          complete: function() {
+            // complete
+          }
+        })
+    }
 })
