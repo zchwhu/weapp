58671c1339ec426d57b84ad34ab27306797d9d77
zch0301 <291053953@qq.com>
2017-02-19 15:23:00 +0800

    设置页面跳转

--- pages/posts/post.js
+++ pages/posts/post.js
@@ -1,12 +1,27 @@
 var postsData = require('../../data/posts-data.js');
 
 Page({
-    data: {
-        
-    },
-    onLoad: function(){
-        this.setData({
-            postList:postsData.postList
-        });
-    }
+  data: {
+
+  },
+  onLoad: function () {
+    this.setData({
+      postList: postsData.postList
+    });
+  },
+  onPostTap: function (event) {
+    var postId = event.currentTarget.dataset.postid;
+    wx.navigateTo({
+      url: 'post-detail/post-detail?id=' + postId,
+      success: function (res) {
+        // success
+      },
+      fail: function () {
+        // fail
+      },
+      complete: function () {
+        // complete
+      }
+    })
+  }
 })
